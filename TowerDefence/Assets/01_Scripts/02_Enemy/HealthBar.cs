761d53881e9c1c49dc82656aa1ed74f38e3ab349
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2022-09-27 20:46:09 -0500

    Chage health bar Color

--- TowerDefence/Assets/01_Scripts/02_Enemy/HealthBar.cs
+++ TowerDefence/Assets/01_Scripts/02_Enemy/HealthBar.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     private Image _healthBar;
+
+    [SerializeField]private Color _initialColor;
+    [SerializeField]private Color _finalcolor;
  
     [SerializeField]private Damagable_Body _master;
 
@@ -13,8 +16,9 @@ public class HealthBar : MonoBehaviour
     {
         _healthBar = GetComponent<Image>();
     }
-    public void UpdateFillAmount()
+    public void UpdateHealthBar()
     {
         _healthBar.fillAmount = _master.CurrentHealthNormalized;
+        _healthBar.color = Color.Lerp(_initialColor, _finalcolor, _master.CurrentHealthNormalized);
     }
 }
